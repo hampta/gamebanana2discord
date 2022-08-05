066d3de711872c1106947e1ef3ca16d12bf4858e
Artem <56561445+hampta@users.noreply.github.com>
2022-08-05 09:26:50 +0300

    fix

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import getenv
 
 
 BASE_URL = "https://gamebanana.com"
-GAMES_ID = getenv("GAMES_ID").strip().split(",")
+GAMES_ID = getenv("GAMES_ID").replace(" ", "").split(",")
 DOWNLOAD_ICON = 'https://cdn.discordapp.com/attachments/942477980167446538/1004980354819559435/icon_sheet.png'
 DATABASE_URL = getenv('DATABASE_URL')
 WEBHOOK_URL = getenv('WEBHOOK_URL')
