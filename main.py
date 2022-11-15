8d48584e4f8a3424998cd596d2f371f38bdf6d9c
Artem <56561445+hampta@users.noreply.github.com>
2022-11-15 02:40:22 +0200

    finally

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from src.gb2ds import Gamebanana2Discord
 
 
 if __name__ == "__main__":
-    gb2ds = Gamebanana2Discord()
-    gb2ds.start()
+    try:
+        gb2ds = Gamebanana2Discord()
+        gb2ds.start()
+    except KeyboardInterrupt:
+        print("Exiting...")
+        exit(0)
